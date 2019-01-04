ee7ea75161239e899bff4c464788c5ed44f88438
OskarHulter <stresslessness91@gmail.com>
2019-01-04 14:25:33 +0100

    added TS, tslint and lint config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,16 @@
 const config = require('./src/utils/siteConfig')
 const path = require(`path`)
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+exports.createPages = ({
+    graphql,
+    actions
+}) => {
+    const {
+        createPage
+    } = actions
 
-  const loadPosts = new Promise((resolve, reject) => {
-    graphql(`
+    const loadPosts = new Promise((resolve, reject) => {
+        graphql(`
       {
         allContentfulPost(
           sort: { fields: [publishDate], order: DESC }
@@ -20,59 +25,61 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     `).then(result => {
-      const posts = result.data.allContentfulPost.edges
-      const postsPerFirstPage = config.postsPerHomePage
-      const postsPerPage = config.postsPerPage
-      const numPages = Math.ceil(
-        posts.slice(postsPerFirstPage).length / postsPerPage
-      )
+            const posts = result.data.allContentfulPost.edges
+            const postsPerFirstPage = config.postsPerHomePage
+            const postsPerPage = config.postsPerPage
+            const numPages = Math.ceil(
+                posts.slice(postsPerFirstPage).length / postsPerPage
+            )
 
-      // Create main home page
-      createPage({
-        path: `/`,
-        component: path.resolve(`./src/templates/index.js`),
-        context: {
-          limit: postsPerFirstPage,
-          skip: 0,
-          numPages: numPages + 1,
-          currentPage: 1,
-        },
-      })
+            // Create main home page
+            createPage({
+                path: `/`,
+                component: path.resolve(`./src/templates/index.js`),
+                context: {
+                    limit: postsPerFirstPage,
+                    skip: 0,
+                    numPages: numPages + 1,
+                    currentPage: 1,
+                },
+            })
 
-      // Create additional pagination on home page if needed
-      Array.from({ length: numPages }).forEach((_, i) => {
-        createPage({
-          path: `/${i + 2}/`,
-          component: path.resolve(`./src/templates/index.js`),
-          context: {
-            limit: postsPerPage,
-            skip: i * postsPerPage + postsPerFirstPage,
-            numPages: numPages + 1,
-            currentPage: i + 2,
-          },
+            // Create additional pagination on home page if needed
+            Array.from({
+                length: numPages
+            }).forEach((_, i) => {
+                createPage({
+                    path: `/${i + 2}/`,
+                    component: path.resolve(`./src/templates/index.js`),
+                    context: {
+                        limit: postsPerPage,
+                        skip: i * postsPerPage + postsPerFirstPage,
+                        numPages: numPages + 1,
+                        currentPage: i + 2,
+                    },
+                })
+            })
+
+            // Create each individual post
+            posts.forEach((edge, i) => {
+                const prev = i === 0 ? null : posts[i - 1].node
+                const next = i === posts.length - 1 ? null : posts[i + 1].node
+                createPage({
+                    path: `${edge.node.slug}/`,
+                    component: path.resolve(`./src/templates/post.js`),
+                    context: {
+                        slug: edge.node.slug,
+                        prev,
+                        next,
+                    },
+                })
+            })
+            resolve()
         })
-      })
-
-      // Create each individual post
-      posts.forEach((edge, i) => {
-        const prev = i === 0 ? null : posts[i - 1].node
-        const next = i === posts.length - 1 ? null : posts[i + 1].node
-        createPage({
-          path: `${edge.node.slug}/`,
-          component: path.resolve(`./src/templates/post.js`),
-          context: {
-            slug: edge.node.slug,
-            prev,
-            next,
-          },
-        })
-      })
-      resolve()
     })
-  })
 
-  const loadTags = new Promise((resolve, reject) => {
-    graphql(`
+    const loadTags = new Promise((resolve, reject) => {
+        graphql(`
       {
         allContentfulTag {
           edges {
@@ -86,34 +93,37 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     `).then(result => {
-      const tags = result.data.allContentfulTag.edges
-      const postsPerPage = config.postsPerPage
+            const tags = result.data.allContentfulTag.edges
+            const postsPerPage = config.postsPerPage
 
-      // Create tag pages with pagination if needed
-      tags.map(({ node }) => {
-        const totalPosts = node.post !== null ? node.post.length : 0
-        const numPages = Math.ceil(totalPosts / postsPerPage)
-        Array.from({ length: numPages }).forEach((_, i) => {
-          createPage({
-            path:
-              i === 0 ? `/tag/${node.slug}/` : `/tag/${node.slug}/${i + 1}/`,
-            component: path.resolve(`./src/templates/tag.js`),
-            context: {
-              slug: node.slug,
-              limit: postsPerPage,
-              skip: i * postsPerPage,
-              numPages: numPages,
-              currentPage: i + 1,
-            },
-          })
+            // Create tag pages with pagination if needed
+            tags.map(({
+                node
+            }) => {
+                const totalPosts = node.post !== null ? node.post.length : 0
+                const numPages = Math.ceil(totalPosts / postsPerPage)
+                Array.from({
+                    length: numPages
+                }).forEach((_, i) => {
+                    createPage({
+                        path: i === 0 ? `/tag/${node.slug}/` : `/tag/${node.slug}/${i + 1}/`,
+                        component: path.resolve(`./src/templates/tag.js`),
+                        context: {
+                            slug: node.slug,
+                            limit: postsPerPage,
+                            skip: i * postsPerPage,
+                            numPages: numPages,
+                            currentPage: i + 1,
+                        },
+                    })
+                })
+            })
+            resolve()
         })
-      })
-      resolve()
     })
-  })
 
-  const loadPages = new Promise((resolve, reject) => {
-    graphql(`
+    const loadPages = new Promise((resolve, reject) => {
+        graphql(`
       {
         allContentfulPage {
           edges {
@@ -124,19 +134,21 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     `).then(result => {
-      const pages = result.data.allContentfulPage.edges
-      pages.map(({ node }) => {
-        createPage({
-          path: `${node.slug}/`,
-          component: path.resolve(`./src/templates/page.js`),
-          context: {
-            slug: node.slug,
-          },
+            const pages = result.data.allContentfulPage.edges
+            pages.map(({
+                node
+            }) => {
+                createPage({
+                    path: `${node.slug}/`,
+                    component: path.resolve(`./src/templates/page.js`),
+                    context: {
+                        slug: node.slug,
+                    },
+                })
+            })
+            resolve()
         })
-      })
-      resolve()
     })
-  })
 
-  return Promise.all([loadPosts, loadTags, loadPages])
+    return Promise.all([loadPosts, loadTags, loadPages])
 }
